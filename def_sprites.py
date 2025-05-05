2344e273d85c5ebb379b5a300f0980b43b30a7d9
Sfibra45 <jpxavier353@gmail.com>
2025-05-05 14:33:38 -0300

    Ideia das animaçoes
    sprite de animação parado , remoção dos sprites antigos e como vai ser a funçoa de sprite

---
+++ def_sprites.py
@@ -0,0 +1,20 @@
+import pygame as pg
+from pygame.locals import *
+pg.init()
+janela=pg.display.set_mode((800,600))
+sprite=pg.image.load("sprite [_parado_teoricamente_centralizado_pngcopia.png")
+sair=False
+x_sprite=0 # velociade
+while not sair:
+# Horiz,vertic,quanto pula, altura sprite, horiz sprite,por quanto tempo vai rolar a imagem
+    janela.blit(sprite,(100,200),(x_sprite*120,40,50,50)) 
+    pg.time.Clock().tick(10)
+    pg.display.flip()
+
+    for e in pg.event.get():
+        if e.type == pg.QUIT or e.type == KEYDOWN and e.key == K_ESCAPE:
+            sair = True
+
+    x_sprite+=1
+    if x_sprite > 10:
+        x_sprite=0
---
+++ multiplos.py
@@ -0,0 +1,15 @@
+# multiplos de X
+x1=float(input("multiplos de x"))
+def m(y):
+    print(x1*y)
+
+m(1)
+m(2)
+m(3)
+m(4)
+m(5)
+m(6)
+m(7)
+m(8)
+m(9)
+m(10)
